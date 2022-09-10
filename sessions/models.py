608fdfc8f5ec7e9fcb41829fb0696c463d4fb474
abbott <51351558+abbotthh@users.noreply.github.com>
2022-09-10 11:52:11 -0600

    Update models.py

--- sessions/models.py
+++ sessions/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 class SessionModel(models.Model):
-    uid1 = models.CharField(max_length=200)
-    uid2 = models.CharField(max_length=200)
-    location1 = models.CharField(max_length=200)
-    location2 = models.CharField(max_length=200)
-    dest1 = models.CharField(max_length=200)
-    dest2 = models.CharField(max_length=200)
-    time1 = models.DateTimeField(auto_now=True)
-    time2 = models.DateTimeField(auto_now=True)
+    id = models.CharField(max_length=200)
+    initiator = models.ForeignKey(UserModel, related_name=("initiator"), on_delete=models.CASCADE, null=True)
+    follower = ArrayField(
+        follower_object = models.ForeignKey(UserModel, related_name=("follower_obj"), on_delete=models.CASCADE, null=True),
+        size=100,
+    )
+    location = models.CharField(max_length=200)
+    time = models.DateTimeField(auto_now=True)
+    success = models.DateTimeField()
     ongoing = models.BooleanField()
