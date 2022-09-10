c06e0f38704e0215ce45ead99093cd5a831e05f2
Yourui Dong <dongismyusername@gmail.com>
2022-09-10 12:18:46 -0600

    intial commit request model and serializer

--- request/views.py
+++ request/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
 
 # Create your views here.
+class RequestView(viewsets.ModelViewSet):
+    serializer_class = RequestSerializer
+    
