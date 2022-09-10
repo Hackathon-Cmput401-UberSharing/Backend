093d717c2721c34410281de027cfd7ea3d90d243
Yourui Dong <dongismyusername@gmail.com>
2022-09-10 13:30:56 -0600

    added views.py

--- request/views.py
+++ request/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import *
 from .models import *
 
 # Create your views here.
-class RequestView(viewsets.ModelViewSet):
-    serializer_class = RequestSerializer
+@api_view(['GET', 'POST'])
+def getRequests(request):
+    if request.method = 'GET':
+        requests = Request.objects.all()
+        serializer = ItemSerializer(requests, many=True)
+        return Response(serializer.data)
+
+@api_view(['POST'])
+def acceptRequest(request):
+    if request.method = 'POST':
+        result = views.acceptRequest(request)
+
+def rejectRequest(request):
+    if request.method = 'POST':
+        result = views.rejectRequest(request)
+
+
     
