dfcda9d9c724b3d63dfe29aec8d74be40c9fb18d
CharlotteZ <simiao4@ualberta.ca>
2022-09-10 11:36:39 -0600

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
 
 # Create your models here.
+class UserModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    profileImage = models.ImageField(verbose_name='profile_image',upload_to='',null=True)
+    displayName = models.CharField(max_length=200)
+    
+
+
+
