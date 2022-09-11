dbb5ab0fde870819708285b8858aaaf25ed67960
Yourui Dong <dongismyusername@gmail.com>
2022-09-11 10:28:15 -0600

    added new requests for groupSessions

--- groupSessions/models.py
+++ groupSessions/models.py
@@ -7,15 +7,17 @@ from users.models import *
 # Create your models here.
 class GroupSessionsModel(models.Model):
     id = models.CharField(max_length=200,primary_key=True)
-    initiator = models.ForeignKey(UserModel, related_name=("initiator"), on_delete=models.CASCADE, null=True)
-    follower = models.ManyToManyField(UserModel)
+    initiatorStr = models.CharField(max_length=200, null=True)
+    #initiator = models.ForeignKey(UserModel, related_name=("initiator"), on_delete=models.CASCADE, null=True)
+    #otherRiders = models.ManyToManyField(UserModel)
     '''
     follower = ArrayField(
         follower_object = models.ForeignKey(UserModel, related_name=("follower_obj"), on_delete=models.CASCADE, null=True),
         size=100,
     )
     '''
-    location = models.CharField(max_length=200)
-    time = models.DateTimeField(auto_now=True)
-    success = models.DateTimeField()
+    destination = models.CharField(max_length=200, null=True)
+    currentLocation = models.CharField(max_length=200, null=True)
+    time = models.DateTimeField(auto_now=True, null=True)
+    success = models.BooleanField()
     ongoing = models.BooleanField()
